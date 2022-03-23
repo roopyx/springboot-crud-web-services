427fca514c192e0cc6dd958c206a64073ff08743
roopyx <roopyx.1111@gmail.com>
2022-03-23 12:15:52 -0300

    first commit

---
+++ src/main/java/com/roopyx/springbootcrudrestfullwebservices/SpringbootCrudRestfullWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.roopyx.springbootcrudrestfullwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudRestfullWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudRestfullWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roopyx/springbootcrudrestfullwebservices/SpringbootCrudRestfullWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roopyx.springbootcrudrestfullwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudRestfullWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
